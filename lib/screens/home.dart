432aac82b5ca90790c82d90bd7e99af6ba730b53
hp <1m2i3r4h@gmail.com>
2023-08-17 16:55:34 +0300

    + onboarding screens + login screen + sign_in screen + home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/shared/components/categories.dart';
 import 'package:grocery_app/shared/components/login_comp.dart';
 
 class HomePage extends StatelessWidget {
+  List<ItemModelRow> itemRow = [
+    ItemModelRow(
+        name: "Vegetables",
+        background: Color(0xFFE6F2EA),
+        urlImage: "assets/img/veg.png"),
+    ItemModelRow(
+        name: "Fruits",
+        background: Color(0xFFFFE9E5),
+        urlImage: "assets/img/fru.png"),
+    ItemModelRow(
+        name: "Vegetables",
+        background: Color(0xFFE6F2EA),
+        urlImage: "assets/img/veg.png"),
+    ItemModelRow(
+        name: "Beverages",
+        background: Color(0xFFFFF6E3),
+        urlImage: "assets/img/bev.png"),
+    ItemModelRow(
+        name: "Grocery",
+        background: Color(0xFFF3EFFA),
+        urlImage: "assets/img/gro.png"),
+    ItemModelRow(
+        name: "Edible oil",
+        background: Color(0xFFDCF4F5),
+        urlImage: "assets/img/edi.png"),
+    ItemModelRow(
+        name: "Household",
+        background: Color(0xFFFFE8F2),
+        urlImage: "assets/img/flo.png"),
+  ];
+  List<ItemModelColumn> categoryItem = [
+    ItemModelColumn(
+      name: "Fresh Peach",
+      price: "\$ 220",
+      urlImage: "assets/img/fresh.png",
+    ),
+    ItemModelColumn(
+      name: "Fresh Peach",
+      price: "\$ 220",
+      urlImage: "assets/img/fresh.png",
+    ),
+    ItemModelColumn(
+      name: "Fresh Peach",
+      price: "\$ 220",
+      urlImage: "assets/img/fresh.png",
+    ),
+    ItemModelColumn(
+      name: "Fresh Peach",
+      price: "\$ 220",
+      urlImage: "assets/img/fresh.png",
+    ),
+    ItemModelColumn(
+      name: "Fresh Peach",
+      price: "\$ 220",
+      urlImage: "assets/img/fresh.png",
+    ),
+    ItemModelColumn(
+      name: "Fresh Peach",
+      price: "\$ 220",
+      urlImage: "assets/img/fresh.png",
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,19 +115,74 @@ class HomePage extends StatelessWidget {
                 IconButton(
                   icon: const Icon(
                     Icons.navigate_next_outlined,
-                    size: 30,
+                    size: 25,
                   ),
                   onPressed: () {},
                 )
               ],
             ),
-            ListView.separated(
-                itemBuilder: itemBuilder(),
-                separatorBuilder: separatorBuilder,
-                itemCount: itemCount)
+            Expanded(
+              flex: 1,
+              child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) =>
+                      itemRowCategory(itemRow[index]),
+                  separatorBuilder: (context, index) => Container(
+                        width: 10,
+                      ),
+                  itemCount: itemRow.length),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Featured products",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.navigate_next_outlined,
+                    size: 25,
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
+            Expanded(
+              flex: 2,
+              child: GridView.builder(
+                  scrollDirection: Axis.vertical,
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 200,
+                      childAspectRatio: 6 / 5,
+                      crossAxisSpacing: 5,
+                      mainAxisExtent: 300,
+                      mainAxisSpacing: 5),
+                  itemCount: categoryItem.length,
+                  itemBuilder: (context, index) =>
+                      itemColumnCategory(categoryItem[index])),
+            ),
           ],
         ),
       ),
     );
   }
+}
+
+class ItemModelRow {
+  String urlImage;
+  Color? background;
+  String name;
+
+  ItemModelRow(
+      {required this.name, required this.background, required this.urlImage});
+}
+
+class ItemModelColumn {
+  String urlImage;
+  String name;
+  String? price;
+
+  ItemModelColumn(
+      {required this.name, required this.price, required this.urlImage});
 }
